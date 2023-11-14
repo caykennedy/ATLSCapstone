cf8e7e294a822a1b3c0eca7018af954ed9d2a6b2
Torshawna Chew <82400607+toch6278-24@users.noreply.github.com>
2023-11-14 13:23:57 -0700

    add textures and traversal

--- FeasibilityTests/Assets/Scripts/CameraTraversal.cs
+++ FeasibilityTests/Assets/Scripts/CameraTraversal.cs
@@ -8,7 +8,7 @@ public class CameraTraversal : MonoBehaviour
     public int bottomBoundary = 5; 
 
     public float direction = 1; 
-    public float multiplier = 1f; 
+    public float multiplier = 90; 
 
     public GameObject person;
 
@@ -21,6 +21,28 @@ public class CameraTraversal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * multiplier);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * multiplier);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0f, 0.2f, 0f);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0f, -0.2f, 0f);
+        }
+        // Movement();
+        
+    }
+
+    void Movement()
+    {
         if(person.transform.position.x < topBoundary)
         {
             Debug.Log("moving down");
@@ -32,6 +54,5 @@ public class CameraTraversal : MonoBehaviour
             direction = 1;
         }
         person.transform.position += new Vector3(1f, 0) * Time.deltaTime * multiplier * direction;
-        
     }
 }
