dbf81bc83cf343c8f6664aca980ccc9af33c3619
Torshawna Chew <71048113+toch6278@users.noreply.github.com>
2024-02-11 00:48:12 -0700

    building fairy town
    fixed button selections
    added more prefabs to make fairy town
    added particle system

--- FeasibilityTests/Assets/Scripts/Options.cs
+++ FeasibilityTests/Assets/Scripts/Options.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.ParticleSystem;
 
 public class Options : MonoBehaviour
 {
+    public GameObject user; 
+    public GameObject tree; 
+    public GameObject ui;
+
     public GameObject option1; 
     public GameObject option2;
+
+    // ParticleSystem fairyDust = GetComponent<ParticleSystem>();
 
     public GameObject[] objectArray; 
 
@@ -35,17 +42,30 @@ public class Options : MonoBehaviour
     public void showFairyTown()
     {
 
-        if(option1.activeInHierarchy == false)
+        if(Input.GetKeyDown("1"))
         {
             option1.SetActive(true);
+            Debug.Log("showing Fairy Town");
+            tree.SetActive(false); 
+            ui.SetActive(false);
+            user.transform.Rotate(0f, -45f, 0);
+
+            // start playing particle system
+            var fairyDust = GetComponent<ParticleSystem>(); 
+            fairyDust.Play(); 
+            Destroy(gameObject, fairyDust.main.duration);
         }
     }
 
     public void showPathOfWisps()
     {
-        if(option2.activeInHierarchy == false)
+        if(Input.GetKeyDown("2"))
         {
             option2.SetActive(true);
+            Debug.Log("showing Path of Wisps");
+            tree.SetActive(false); 
+            ui.SetActive(false);
+            user.transform.Rotate(0f, 90f, 0);
         }
     }
 }
