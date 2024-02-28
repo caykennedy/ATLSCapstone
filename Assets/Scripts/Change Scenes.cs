944573555d078074f40db25d50f34d39d70de219
Torshawna Chew <71048113+toch6278@users.noreply.github.com>
2024-02-27 22:01:12 -0700

    remove Feasibility Test folder

---
+++ Assets/Scripts/Change Scenes.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// https://www.youtube.com/watch?v=jrPTpD2eAMw
+
+public class ChangeScenes : MonoBehaviour
+{
+    // public static SceneManager Instance;
+    public string sceneName;
+    public AudioSource clip; 
+
+    
+    // private void Awake()
+    // {
+    //     // access class and manager from this Instance 
+    //     Instance = this; 
+    // }
+
+    // //constants that cannot change
+    // public enum SceneManagement
+    // {
+    //     // need to have same order as in Build Setting
+    //     WelcomeScene,
+    //     StartingScene
+    // }
+
+    // public void LoadScene(Scene scene)
+    // {
+    //     SceneManager.LoadScene(scene.ToString());
+    // }
+
+    // public void LoadStartScene()
+    // {
+    //     SceneManager.LoadScene(Scene.StartingScene.ToString());
+    // }
+
+    // public void LoadNextScene()
+    // {
+    //     // going into build scene and loading the next consecutive scene in list
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    // }
+
+    // public void LoadWelcomeScene()
+    // {
+    //     SceneManager.LoadScene(Scene.WelcomeScene.ToString());
+    // }
+    void Update()
+    {
+        enchantedWoods();
+    }
+    
+    // public void changeScene()
+    // {
+    //     SceneManager.LoadScene(sceneName);
+    //     clip.Play();
+    // }
+
+    // void OnTriggerEnter (Collider player)
+    // {
+    //     Debug.Log("entering");
+    //     changeScene();
+    // }
+
+    public void enchantedWoods()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            SceneManager.LoadScene("Enchanted Woods");
+        }
+    }
+}
