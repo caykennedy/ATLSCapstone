36f1deca1e0d6c94a3e34151cf529ee7e1b9b983
caykennedy <111911400+caykennedy@users.noreply.github.com>
2024-04-29 08:07:04 -0600

    populated opening scene a bit

--- Assets/Scripts/billboard.cs
+++ Assets/Scripts/billboard.cs
@@ -1,9 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    void Update() 
+    [Tooltip("Locks the rotational up axis of the billboard.")]
+    public bool lockUpAxis = true;
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        transform.LookAt(Camera.main.transform.position);
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lockUpAxis)
+        {
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, camera.transform.eulerAngles.y + 180, gameObject.transform.eulerAngles.z);
+        }
+        else {
+            gameObject.transform.LookAt(camera.transform.position);
+        }
     }
 }
